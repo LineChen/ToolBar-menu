0f7fea732c1ba22861c7ee68047a57a8fc6a82a8
Beiing <15764230067@163.com>
2017-08-11 11:40:10 +0800

    update

--- app/src/main/java/com/beiing/toolbar_menu/MyApplication.java
+++ app/src/main/java/com/beiing/toolbar_menu/MyApplication.java
@@ -1,10 +1,24 @@
 package com.beiing.toolbar_menu;
 
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
 /**
  * Created by linechen on 2017/8/8.<br/>
  * 描述：
  * </br>
  */
 
-public class MyApplication {
+public class MyApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(
+                                Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(
+                                Stetho.defaultInspectorModulesProvider(this))
+                        .build());
+    }
 }
